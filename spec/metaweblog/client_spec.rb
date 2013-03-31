69b04e38fbabd967324d68436b9f4c9f17429620
Tatsuya Sato <tatsuya.b.sato@mail.rakuten.com>
2013-03-31 20:18:06 +0900

    Client#post posts new entry

--- spec/metaweblog/client_spec.rb
+++ spec/metaweblog/client_spec.rb
@@ -3,5 +3,24 @@
 require 'spec_helper'
 
 describe MetaWeblog::Client do
-
+  before do
+    @client = MetaWeblog::Client.new('http://blog.example.com/xmlrpc',
+                                     'blog_id',
+                                     'username',
+                                     'password')
+    @xml_client = stub()
+    @client.instance_variable_set(:@client, @xml_client)
+  end
+  describe "#post" do
+    let(:expected_post) { MetaWeblog::Post.new('title', 'hogehoge') }
+    before do
+      @xml_client.should_receive(:call).with('metaWeblog.newPost',
+                                             'blog_id',
+                                             'username', 'password',
+                                             expected_post, true)
+    end
+    it do
+      expect { @client.post(expected_post) }.not_to raise_error
+    end
+  end
 end
