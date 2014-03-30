725296420f6a38177f7c8f73526a096c70926028
Tatsuya Sato <tatsuya.b.sato@mail.rakuten.com>
2014-03-30 19:28:32 +0900

    MetaWeblog::Post#new allows to use hash

--- lib/metaweblog/post.rb
+++ lib/metaweblog/post.rb
@@ -1,4 +1,18 @@
+# encoding: utf-8
+
 module MetaWeblog
   POST_MEMBERS = [:title, :link, :description]
-  Post = Struct.new(*POST_MEMBERS)
+
+  Post = Struct.new(*POST_MEMBERS) do
+    def initialize(*args)
+      if args.length == 1 && args.last.is_a?(Hash)
+        h = args.last
+        self.members.each do |member|
+          self[member] = h[member]
+        end
+      else
+        super
+      end
+    end
+  end
 end
---
+++ spec/metaweblog/post_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe MetaWeblog::Post do
+  describe '#initialize' do
+    let(:post) { MetaWeblog::Post.new(*args) }
+
+    context 'Given an array of values' do
+      let(:args) do
+        [ 'title', 'http://blog.example.com/1234', 'desc' ]
+      end
+
+      specify '' do
+        expect(post.title).to eq('title')
+        expect(post.link).to eq('http://blog.example.com/1234')
+        expect(post.description).to eq('desc')
+      end
+    end
+
+    context 'Given only a hash' do
+      let(:args) do
+        [{ :title => 'Hello', :description => 'This is blog', :extra => 'not defined' }]
+      end
+
+      specify 'should assign values for members' do
+        MetaWeblog::POST_MEMBERS.each do |member|
+          expect(post[member]).to eq(args[0][member])
+        end
+      end
+    end
+  end
+
+  describe '#pubDate' do
+  end
+end
